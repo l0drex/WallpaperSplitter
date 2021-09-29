e66a48d9a201daeafce15f4280fa71f37fdaa153
l0drex <l0drex@pm.me>
2021-09-29 15:06:37 +0200

    GUI version will only start if no arguments are provided

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <QApplication>
 #include "wallpapersplitter.h"
 
+QCoreApplication* createApplication(int &argc, char *argv[]) {
+    if (argc <= 1) {
+        return new QApplication(argc, argv);
+    }
+    return new QCoreApplication(argc, argv);
+}
+
 int main(int argc, char *argv[]) {
-    // TODO add support for non-gui version
-    QApplication a(argc, argv);
+    // parts of this source code where taken from here: https://doc.qt.io/qt-5/qapplication.html
+    QScopedPointer<QCoreApplication> app(createApplication(argc, argv));
 
-    WallpaperSplitter splitter;
-    splitter.show();
+    if (qobject_cast<QApplication *>(app.data())) {
+        // start GUI
+        WallpaperSplitter splitter;
+        splitter.show();
 
-    return QApplication::exec();
+        return QApplication::exec();
+    } else {
+        // TODO parse arguments and split according to them
+    }
 }
